5832878a80b824b44224f705c16bcb3102b9736d
문주연 <>
2025-05-06 02:11:51 +0900

    스타트 화면 넣어둠

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-//import React from 'react';
-import {Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { SurveyProvider } from './content/SurveyContext';
 
 import GoalPage from './pages/GoalPage';
@@ -8,30 +7,28 @@ import RoutineTime from './pages/RoutineTime';
 import PersonalityPage from './pages/PersonalityPage';
 import EmotionPage from './pages/EmotionPage';
 import SummaryPage from './pages/SummaryPage';
-import RoutineListPage from './pages/RoutineListPage';
+import RoutineSelectionPage from './pages/RoutineSelectionPage';
 import RoutineEditPage from './pages/RoutineEditPage';
-
+import RoutineOpinionPage from './pages/RoutineOpinionPage';
+import RoutineStartPage from "./pages/RoutineStartPage.tsx";
 
 const App = () => {
-    // App.tsx
     return (
         <SurveyProvider>
             <Routes>
-                <Route path="/" element={<GoalPage/>}/>
-                <Route path="/time" element={<TimePage/>}/>
-                <Route path="/time-duration" element={<RoutineTime/>}/>
-                <Route path="/personality" element={<PersonalityPage/>}/>
-                <Route path="/emotion" element={<EmotionPage/>}/>
-                <Route path="/summary" element={<SummaryPage/>}/>
-                <Route path="/routines" element={<RoutineListPage />} />
-                <Route path="/routines/:id/edit" element={<RoutineEditPage />} />
-
-
+                <Route path="/" element={<GoalPage />} />
+                <Route path="/time" element={<TimePage />} />
+                <Route path="/time-duration" element={<RoutineTime />} />
+                <Route path="/personality" element={<PersonalityPage />} />
+                <Route path="/emotion" element={<EmotionPage />} />
+                <Route path="/summary" element={<SummaryPage />} />
+                <Route path="/routine-opinion" element={<RoutineOpinionPage />} />
+                <Route path="/routines" element={<RoutineSelectionPage />} />
+                <Route path="/routine-customize" element={<RoutineEditPage />} />
+                <Route path="/main" element={<RoutineStartPage />} />
             </Routes>
         </SurveyProvider>
     );
-}
+};
 
-
-
-    export default App;
+export default App;
